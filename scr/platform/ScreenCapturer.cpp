73adcc46cb07485451cd9d9e8632640f507f4c1e
Clicedvly768 <harepro29@gmail.com>
2026-03-31 23:50:57 +0500

    Changes to be committed: 	new file:   include/platform/ScreenCapturer.h 	new file:   scr/platform/ScreenCapturer.cpp
     Changes not staged for commit:
    	modified:   include/app/UnifiedMessenger.h
    	modified:   include/ui/Interface.h
    	modified:   meson.build
    	modified:   scr/app/UnifiedMessenger.cpp
    	modified:   scr/main.cpp
    
     Untracked files:
    	.vs/

---
+++ scr/platform/ScreenCapturer.cpp
@@ -0,0 +1,11 @@
+#include "platform/ScreenCapturer.h"
+
+std::vector<ScreenCapturer::Screen> ScreenCapturer::get_screens() {
+    // Platform-specific implementation needed
+    return {};
+}
+
+ScreenCapturer::Image ScreenCapturer::capture_screen(int screen_id) {
+    // Platform-specific implementation needed
+    return {};
+}
